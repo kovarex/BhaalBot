f5dbeeb1acfc803243ac8a8e405529e26eaaedc3
kovarex <kovarex@gmail.com>
2017-01-21 11:12:01 +0100

    Assignment class comments.

--- src/Assignment.hpp
+++ src/Assignment.hpp
@@ -2,11 +2,13 @@
 #include <string>
 class Unit;
 
+/** Specification of unit assignment.
+ * It can be used to read what the assignment is and to unassign the unit from its task whatever it is. */
 class Assignment
 {
 public:
-  virtual ~Assignment() {}
-  virtual std::string str() const = 0;
+  virtual ~Assignment() {} /**< Automatically unassigns the unit from its task. */
+  virtual std::string str() const = 0; /**< @return String representation of the assignment */
 
-  Unit* unit = nullptr;
+  Unit* unit = nullptr; /**< The unit witht the assignement. This is set it Unit::assign. */
 };
